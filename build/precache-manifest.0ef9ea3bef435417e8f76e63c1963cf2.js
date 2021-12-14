62d3f46b8f5d68f7409e57e2e61e5ea392596c79
thales <thalesdaniel2013@gmail.com>
2021-12-14 01:15:16 -0300

    dado build

---
+++ build/precache-manifest.0ef9ea3bef435417e8f76e63c1963cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ecff8a97bccecd8ababf4b87a7b5be",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "bc7e3d5173f55dbcbdeb",
+    "url": "/trivia-game/static/css/main.6fda0aea.chunk.css"
+  },
+  {
+    "revision": "8fa7988462ec8ec0f027",
+    "url": "/trivia-game/static/js/2.f25a6682.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trivia-game/static/js/2.f25a6682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc7e3d5173f55dbcbdeb",
+    "url": "/trivia-game/static/js/main.2280b632.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  }
+]);
