78dbd6a05c3d6b9ac8cc6da2f24c439776963dd1
thales <thalesdaniel2013@gmail.com>
2021-12-14 18:26:23 -0300

    Updates

---
+++ precache-manifest.a73c2bc6f048b0d262bc97bd50e37584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a8ddea8b20f36f395fb18f7a120408",
+    "url": "/Trivia-Game/index.html"
+  },
+  {
+    "revision": "bc7e3d5173f55dbcbdeb",
+    "url": "/Trivia-Game/static/css/main.6fda0aea.chunk.css"
+  },
+  {
+    "revision": "8fa7988462ec8ec0f027",
+    "url": "/Trivia-Game/static/js/2.f25a6682.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Trivia-Game/static/js/2.f25a6682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc7e3d5173f55dbcbdeb",
+    "url": "/Trivia-Game/static/js/main.2280b632.chunk.js"
+  },
+  {
+    "revision": "664fd19e6cdf71782b6d",
+    "url": "/Trivia-Game/static/js/runtime-main.706366f1.js"
+  }
+]);
